52b3cf399f45904ddfdefe4c23132aeb751e86c2
David Snopek <dsnopek@gmail.com>
2025-09-17 17:31:26 -0500

    Actually exec commands on request!

--- app/src/main/java/org/godotengine/godot_gradle_build_environment/BuildEnvironmentService.kt
+++ app/src/main/java/org/godotengine/godot_gradle_build_environment/BuildEnvironmentService.kt
@@ -7,20 +7,45 @@ import android.os.Handler
 import android.os.IBinder
 import android.os.Message
 import android.os.Messenger
+import android.util.Log
 import android.widget.Toast
+import java.io.File
 
-private const val MSG_SAY_HELLO = 1
+private const val MSG_EXECUTE_COMMAND = 1
+private const val MSG_SAY_HELLO = 2
 
 class BuildEnvironmentService : Service() {
 
     private lateinit var mMessager: Messenger
 
+    private lateinit var mEmbeddedLinux: EmbeddedLinux
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val debianRootfs = File(filesDir, "rootfs/alpine-android-35-jdk17")
+        mEmbeddedLinux = EmbeddedLinux(this, debianRootfs.absolutePath)
+    }
+
     internal class IncomingHandler(
-        context: Context,
-        private val applicationContext: Context = context.applicationContext
+        private val parent: BuildEnvironmentService,
+        private val applicationContext: Context = parent.applicationContext
     ): Handler() {
         override fun handleMessage(msg: Message) {
             when (msg.what) {
+                MSG_EXECUTE_COMMAND -> {
+                    Log.i("DRS", "recieved MSG_EXECUTE_COMMAND")
+                    val data = msg.data
+                    val path = data.getString("path")
+                    //val workDir = data.getString("workDir")
+                    val args = data.getStringArrayList("args")
+
+                    if (path != null && args != null) {
+                        args.add(0, path)
+                        val result = parent.mEmbeddedLinux.exec(args)
+                        Log.i("DRS", "We execed a command on request and got: $result")
+                    }
+                }
                 MSG_SAY_HELLO ->
                     Toast.makeText(applicationContext, "hello!", Toast.LENGTH_SHORT).show()
                 else -> super.handleMessage(msg)
